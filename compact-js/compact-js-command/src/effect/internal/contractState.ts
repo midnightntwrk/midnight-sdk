3cbb322f80856926d583a09df6446ff23ddc6fd6
Tim Roberts <tim.roberts@shielded.io>
2026-02-12 15:04:23 +0000

    Update to Ledger 8

--- compact-js/compact-js-command/src/effect/internal/contractState.ts
+++ compact-js/compact-js-command/src/effect/internal/contractState.ts
@@ -15,7 +15,7 @@
 
 import { ContractRuntimeError } from '@midnight-ntwrk/compact-js/effect';
 import { ContractState } from '@midnight-ntwrk/compact-runtime';
-import { ContractState as LedgerContractState } from '@midnight-ntwrk/ledger-v7';
+import { ContractState as LedgerContractState } from '@midnight-ntwrk/ledger-v8';
 import { Effect } from 'effect';
 
 /** @internal */
